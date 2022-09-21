a0924a965974ad781164d4a17d74dcd7e60981da
Ankit <iamnakitsingh24@gmail.com>
2022-09-21 13:53:26 +0530

    3rd chapter

---
+++ chapter03practice.py
@@ -0,0 +1,18 @@
+'''a = input("Enter a name")
+print ("Good morning"+a)'''
+# letter = '''Dear <|NAME|>
+# You are selected
+# <|DATE|>'''
+# name = input("Enter your name")
+# date = input("Enter a date")
+# letter = letter.replace("<|NAME|>", name)
+# letter = letter.replace("<|DATE|>", date)
+# print(letter)
+'''st = "This line contains  double spaces"
+# doublespace = st.find("  ")
+st = st.replace("  "," ")
+print(st)'''
+letter = "Dear Harry, Thank you for your efforts. Thanks !"
+print(letter)
+formatted_letter = "Dear Harry,\n\tThank you for your efforts. \nThanks !"
+print(formatted_letter)
