9efec4d8ae92c391b5af43f5d42e3a885ca186e1
Ankit <iamnakitsingh24@gmail.com>
2022-09-21 13:44:21 +0530

    Datatype file changes

--- Datatype.py
+++ Datatype.py
@@ -1,6 +1,10 @@
-a = 5 #int
+'''a = 5 #int
 b = 9.8 #float
 c = True #boolean
-d = "Code" # string can be written as 'Code' or '''Code'''
+d = "Code" # string can be written as 'Code'
 print(a, b, c, d)
-print(type(a))
+print(type(a))'''
+
+a = 5 
+b = 6
+print("Addition of a and b is :", a+b)
