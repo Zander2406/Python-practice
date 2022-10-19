63cba695976191488b72115581afc8e5a1c124ed
Ankit <iamnakitsingh24@gmail.com>
2022-10-19 14:54:07 +0530

    concept of function and recursion

---
+++ funtions.py
@@ -0,0 +1,77 @@
+'''def CelsiusToFahrenheit(temperature):
+    print((temperature * (9 / 5)) + 32)
+
+
+def FahrenheitToCelsius(temperature):
+    print((temperature - 32) * (5 / 9))
+
+
+temp = float(input("Enter the temperature: "))
+choice = int(input("1. C -> F\n2. F -> C\n"))
+if choice == 1:
+    CelsiusToFahrenheit(temp)
+else:
+    FahrenheitToCelsius(temp)'''
+
+
+'''def CelsiusToFahrenheit(temperature):
+    return((temperature * (9 / 5)) + 32)
+
+
+def FahrenheitToCelsius(temperature):
+    return((temperature - 32) * (5 / 9))
+
+
+temp = float(input("Enter the temperature: "))
+choice = int(input("1. C -> F\n2. F -> C\n"))
+if choice == 1:
+    temp = CelsiusToFahrenheit(temp)
+else:
+    temp = FahrenheitToCelsius(temp)
+
+print(temp)
+if temp >= 50:
+    print("Scochares day")'''
+
+def factorial(number):
+    if number == 0 or number == 1:
+        return 1
+    return number * factorial(number-1) 
+
+
+num = int(input("Enter a digit = "))
+fact = factorial(num)
+print(fact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
